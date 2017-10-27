4c519d96c22e2d00bb169781ace1161e4f8855b9
Nataliya Dubrovska <n.dubrovska89@gmail.com>
2017-10-27 18:21:29 +0100

    Add webpack setup for React bundling

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: './src/client/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_mudules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env', 'react']
+                    }
+                }    
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin()
+    ]
+};
